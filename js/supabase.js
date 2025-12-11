5fcd440d465f99b7b6e8ccc1e923ab393dc0bc0c
Momade Junior <momadejunior584@gmail.com>
2025-12-11 08:13:06 +0200

    and dashboard to the website

---
+++ js/supabase.js
@@ -0,0 +1,3 @@
+// Substitui pelos teus dados do Supabase
+        const SUPABASE_URL = "https://lkfmtfihkbkjhigrajwb.supabase.co";
+        const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZm10Zmloa2JramhpZ3JhandiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MDIxOTgsImV4cCI6MjA3ODQ3ODE5OH0.J07lM8E3CsMwvbnDXxeXvTboL46-jqa00jDKTJ2mKHM";
